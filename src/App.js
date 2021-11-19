f8ed8714f8b0143909c96b359af1e6ab0ade6767
yinsuyinsusu <yinsu7@outlook.com>
2021-11-19 21:15:42 +0000

    typo update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import Dictionary from "./Dictionary.js";
 
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Dictionary />
-        <p>search a word...</p>
+        <p>search for a word...</p>
       </header>
     </div>
   );
